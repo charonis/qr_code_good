5b5a8d6b8afb47ba722cfa6020511f3e6a27c0f4
dfsfd <dfkjakfja>
2024-08-26 00:55:22 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ app = FastAPI(title="qr-code")
 
 
 origins = [
-    "https://qr-code-front-three.vercel.app"
+    "https://qr-code-front-three.vercel.app",
+    "http://localhost:5173"
 ]
 
 app.add_middleware(
@@ -17,7 +18,7 @@ app.add_middleware(
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=[ "*" ]
+    allow_headers=["*"]
 )
 
 app.include_router(router_qr)
