d4686a05ac3bbd51dfde0960cb522c467c9cd24c
dfsfd <dfkjakfja>
2024-08-25 19:59:45 +0300

    new file:   .gitignore 	new file:   config.py 	new file:   datebase.py 	new file:   main.py 	new file:   models.py 	new file:   orm.py 	new file:   repository.py 	new file:   requirements.txt 	new file:   router/qr_code.py 	new file:   shemas.py 	new file:   utils.py 	new file:   vercel.json

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from router.qr_code import router_qr
+from fastapi.middleware.cors import CORSMiddleware
+
+
+
+
+app = FastAPI(title="qr-code")
+
+
+origins = [
+    "https://qr-code-front-three.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "DELETE", "PUT"],
+    # allow_headers=[ "Access-Control-Allow-Headers", "Access-Control-Allow-Origin", "accept","Content-Type" ],
+    allow_headers=["*"],
+)
+
+app.include_router(router_qr)
+
+
