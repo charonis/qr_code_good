6cba2f17ce496db43dbec9ae604a718e2e46337c
dfsfd <dfkjakfja>
2024-08-26 22:24:11 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -17,8 +17,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
+     allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization","Accept",  ],
 )
 
 app.include_router(router_qr)
