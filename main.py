d4958f412434d58766ead5bb58b7f91141cd72e3
dfsfd <dfkjakfja>
2024-08-31 19:32:55 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-     allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
     allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
                    "Authorization","Accept", "Access-Control-Allow-Methods", 'sec-fetch-mode' ],
 )
